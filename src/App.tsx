290881ecfb05470fd57da3b561355983678894cc
Jiho YANG <yjsm111@nuua.ai>
2023-04-20 16:15:19 +0900

    UPDATE: elete App.css and refactor App.tsx to include a theme calculator with the ability to add and modify color pairs

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Container, Title } from 'components';
+import ColorBox from 'components/ColorBox/ColorBox';
+import { Flex } from 'components/common';
+import { useState } from 'react';
+import { ColorPairType } from 'types/common';
+import { getColor, getColorDistance } from 'utils/color';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [colorPairs, setColorPairs] = useState<
+    Array<ColorPairType & { isStandard: boolean }>
+  >([{ input: '#2e8555', generated: '#eb4034', isStandard: true }]);
+
+  const handleAddButtonClick = () => {
+    setColorPairs((prev) => [
+      ...prev,
+      { input: '', generated: '', isStandard: false },
+    ]);
+  };
+
+  const handleChangeColorInput = (
+    target: 'input' | 'generated',
+    index: number,
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const { value } = e.target;
+    setColorPairs((prev) => {
+      const newColorPairs = [...prev];
+      newColorPairs[index][target] = value;
+      return newColorPairs;
+    });
+  };
+
+  const handleOnClickColorBox = (index: number) => {
+    console.log(index);
+    setColorPairs((prev) => {
+      const newColorPairs = [...prev];
+      newColorPairs.forEach((colorPair, i) => {
+        colorPair.isStandard = i === index;
+      });
+      return newColorPairs;
+    });
+  };
+
+  const primaryDistance = getColorDistance(
+    colorPairs.filter((pair) => pair.isStandard)[0].input,
+    colorPairs.filter((pair) => pair.isStandard)[0].generated,
+  );
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Container>
+      <Title>Theme Calculator</Title>
+      <Flex direction="ROW" gap={{ column: 16 }}>
+        {colorPairs.map((colorPair, index) => (
+          <Flex direction="COLUMN" gap={{ row: 8 }}>
+            <ColorBox
+              onClick={() => handleOnClickColorBox(index)}
+              color={colorPair.input}
+              isStandard={colorPair.isStandard}
+            />
+            <input
+              onChange={(e) => handleChangeColorInput('input', index, e)}
+              value={colorPair.input}
+            />
+            <ColorBox
+              onClick={() => handleOnClickColorBox(index)}
+              color={
+                colorPair.isStandard
+                  ? colorPair.generated
+                  : getColor(
+                      colorPairs.filter((pair) => pair.isStandard)[0].generated,
+                      getColorDistance(
+                        colorPairs.filter((pair) => pair.isStandard)[0].input,
+                        colorPair.input,
+                      ),
+                    )
+              }
+              isStandard={colorPair.isStandard}
+            />
+          </Flex>
+        ))}
+      </Flex>
+      <button onClick={handleAddButtonClick}>색상 추가</button>
+    </Container>
+  );
 }
 
-export default App
+export default App;
---
+++ src/types/common.ts
@@ -0,0 +1,4 @@
+export type ColorPairType = {
+  input: string;
+  generated: string;
+};
