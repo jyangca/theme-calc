37670480099af5eee2ae7dd93406d746a0d819ee
Jiho YANG <yjsm111@nuua.ai>
2023-04-21 09:48:51 +0900

    UPDATE: Add new common components to the index file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
 export { default as Container } from './Container/Container';
-export { default as Title } from './Title/Title';
+export { default as Title } from './common/Title/Title';
+export { default as ContentContainer } from './ContentContainer/ContentContainer';
+export { default as ColorBox } from './ColorBox/ColorBox';
+export { default as AddButton } from './AddButton/AddButton';
