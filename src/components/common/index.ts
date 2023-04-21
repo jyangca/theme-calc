7addd41ff903d4b42d3731fadb9c9634bdde9ffb
Jiho YANG <yjsm111@nuua.ai>
2023-04-21 14:01:27 +0900

    UPDATE: Add Portal and Popover components to common index.ts file

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,5 @@
 export { default as Flex } from './Flex/Flex';
 export { default as Title } from './Title/Title';
 export { default as Input } from './Input/Input';
+export { default as Portal } from './Portal/Portal';
+export { default as Popover } from './Popover/Popover';
