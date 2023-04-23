ddfe499b485a19079fccf9597fc2e86597817c2a
Jiho YANG <yjsm111@nuua.ai>
2023-04-23 20:29:18 +0900

    ADD: Add function to invert hex color values in color.ts module

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -17,3 +17,23 @@ export const getColor = (primary: string, distance: number) =>
 export const isValidHexColor = (color: string) => {
   return /^#[0-9A-F]{6}$/i.test(color);
 };
+
+export const invertHex = (hex: string) => {
+  hex = hex.replace('#', '');
+
+  const r = parseInt(hex.substring(0, 2), 16);
+  const g = parseInt(hex.substring(2, 4), 16);
+  const b = parseInt(hex.substring(4, 6), 16);
+
+  const invertedR = (255 - r).toString(16);
+  const invertedG = (255 - g).toString(16);
+  const invertedB = (255 - b).toString(16);
+
+  const pad = (str: string) => {
+    return str.length === 1 ? '0' + str : str;
+  };
+
+  const invertedHex = `#${pad(invertedR)}${pad(invertedG)}${pad(invertedB)}`;
+
+  return invertedHex;
+};
