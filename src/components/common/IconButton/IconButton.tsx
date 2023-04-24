cbb14fdd094842c47d82f21ef051e8d9c241fae5
Jiho YANG <yjsm111@nuua.ai>
2023-04-24 22:57:36 +0900

    UPDATE: Refactor IconButton component to remove unused React Spring library and simplify code

--- src/components/common/IconButton/IconButton.tsx
+++ src/components/common/IconButton/IconButton.tsx
@@ -1,4 +1,3 @@
-import { animated, useSpring } from '@react-spring/web';
 import { IconWrapper } from './IconButton.style';
 
 type AddButtonProps = {
@@ -6,21 +5,10 @@ type AddButtonProps = {
   children: React.ReactNode;
 };
 const IconButton = ({ onClick, children }: AddButtonProps) => {
-  const iconButtonSpringProps = useSpring({
-    from: { opacity: 0, transform: 'scale(0.5)' },
-    to: {
-      opacity: 1,
-      transform: 'scale(1)',
-    },
-    delay: 100,
-  });
-
   return (
-    <animated.div style={iconButtonSpringProps}>
-      <IconWrapper onClick={onClick} justify="CENTER">
-        {children}
-      </IconWrapper>
-    </animated.div>
+    <IconWrapper onClick={onClick} justify="CENTER">
+      {children}
+    </IconWrapper>
   );
 };
 
